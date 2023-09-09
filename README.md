683a9edcefbf721a13301b48c8d97f5e663affee
vladbarto <utclujvb@gmail.com>
2023-09-09 13:30:48 +0200

    card style update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # neo-sport
+    {
+        "img": "./Place/Images/florinelcoman.jpeg",
+        "title": "Ianis Hagi, la faianta?",
+        "meta": null
+    },
+    {
+        "img":"./Place/Images/car-cu-boi.jpg",
+        "title":"Impresionanta lucrare a lui N. Grigorescu",
+        "meta": null
+    },
+    {
+        "img": "./Place/Images/ianis_hagi.jpg",
+        "title": "Florinel Coman, bijuteria de 100 de milioane",
+        "meta": null
+    }
--- project.js
+++ project.js
@@ -1,2 +1,28 @@
-var msg = 'Hello World';
-console.log(msg);
+let card = document.querySelector('.card-container');
+
+const postMethod = (articlesArray) => {
+
+    articlesArray.map( (item) => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('card');
+        postElement.innerHTML = ` <div class="card-img">
+        <img src="${item.img}" alt="Felix Brych">
+    </div>
+    <div class="card-data">
+        <span>${item.date}</span>
+        <h2>${item.title}</h2>
+    <p>${item.meta}</p>
+    </div>
+    <a href="/Articles/404.html" class="card-link"></a> `;
+    
+        card.append(postElement);
+    })
+}
+
+fetch('articles.json')
+  .then(response => response.json())
+  .then(data => {
+    postMethod(data);
+  })
+
+
